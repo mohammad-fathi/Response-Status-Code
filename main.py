d3433bd445b0da3853b9203dd386022cc5a8c75e
Mohammad Fathi <94295589+mohammad-fathi@users.noreply.github.com>
2023-01-22 17:49:38 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+input_url = input("Enter all URL Site separated by space: ")
+website_url = input_url.split(" ")
+
+statuses = {200: "Website Available",301: "Permanent Redirect",302: "Temporary Redirect",404: "Not Found",500: "Internal Server Error",503: "Service Unavailable"}
+for url in website_url:
+    try:
+        web_response = requests.get(url)
+        print(url, statuses[web_response.status_code])
+    except:
+        print("Could Not DNS Resolve or bad URL")
+
